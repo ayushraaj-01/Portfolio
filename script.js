47eb3a6c23abdb45bead5cea1a81e0c325e9f195
ayushraaj-01 <ayushr94150@gmail.com>
2026-02-25 12:56:30 +0530

    some new changes

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-xmark')
     navbar.classList.toggle('active');
 }
+const themeSwitch = document.getElementById('theme-switch');
+
+// Load saved theme
+if (localStorage.getItem('theme') === 'light') {
+  document.body.classList.add('light-theme');
+  themeSwitch.checked = true;
+}
+
+// Toggle theme
+themeSwitch.addEventListener('change', () => {
+  if (themeSwitch.checked) {
+    document.body.classList.add('light-theme');
+    localStorage.setItem('theme', 'light');
+  } else {
+    document.body.classList.remove('light-theme');
+    localStorage.setItem('theme', 'dark');
+  }
+});
+const body = document.body;
+const toggleBtn = document.getElementById("themeToggle"); // your button or icon
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("light-mode");
+});
